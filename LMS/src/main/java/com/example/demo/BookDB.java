b0d52a833a6db1dc5dc0c562e09e81e17d381298
ashwini <ashwinign.7871@gmail.com>
2023-08-08 07:11:48 +0530

    08/08/23

--- LMS/src/main/java/com/example/demo/BookDB.java
+++ LMS/src/main/java/com/example/demo/BookDB.java
@@ -7,11 +7,11 @@ import org.springframework.data.repository.CrudRepository;
 import jakarta.transaction.Transactional;
 
 public interface BookDB extends CrudRepository<Book, Integer> {
-//	@Transactional
-//    @Modifying
-//    @Query("update Book bk set bk.Bname=?1,bk.Aname=?2,bk.Category=?3,bk.Pname=?4,bk.Edition=?5,bk.Pages=?6,where bk.Isbn=?7")
-//      void updateBook(String bname,String aname,String category,String pname,int edition,int pages, int isbn);
-	
-//	  studentDB.updateStudent(stud.getStudentName(), stud.getRollNo());
-//
+	  @Transactional
+	    @Modifying
+	    @Query("update Book bk set bk.bname=?1,bk.aname=?2,bk.category=?3,bk.pname=?4,bk.edition=?5,bk.pages=?6 where bk.isbn=?7")
+	  void updateBook(String bname,String aname,
+			  				String category,String pname,
+			  				int edition,int pages,int isbn);
+	  
 }
--- LMS/src/main/webapp/addindex.jsp
+++ LMS/src/main/webapp/addindex.jsp
@@ -3,7 +3,6 @@
 <link href="/css/add.css" rel="stylesheet"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<link href="/css/delete.css" rel="stylesheet">
 </head>
 
 <body>
